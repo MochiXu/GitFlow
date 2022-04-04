0922595031063c09297f09d7dc495ba1fe6d13a5
MochiXu <mirage_mc@icloud.com>
2022-04-05 06:30:26 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        //输入处理
+        Scanner scanner=new Scanner(System.in);
+        //读取首行两个int参数
+        int N= scanner.nextInt();
+        int m= scanner.nextInt();
+        scanner.nextLine();
+        //循环读取第二行数组
+        int[]nums=new int[N];
+        for (int i=0;i<N;i++){
+            nums[i]=scanner.nextInt();
+        }
+        //结果处理
+        int ans=0;
+        for (int num:nums){
+            if (num==m)ans++;
+        }
+        //输出结果
+        System.out.println(ans);
+        //init版本
+    }
+}
